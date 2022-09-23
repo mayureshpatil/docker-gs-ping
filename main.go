826f5f5421be047b4fb5b6121d986f1a7790cac1
mayureshpatil <53110327+mayureshpatil@users.noreply.github.com>
2022-09-23 10:34:12 +0530

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, IBM cloud")
+		return c.HTML(http.StatusOK, "Hello, IBM ISDL")
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
